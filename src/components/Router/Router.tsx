19ef3af6bdcf6da1e1fe0c8328c85b820f906b6f
Alexandr Rutkowski <kitanoyoru@protonmail.com>
2023-11-05 17:44:23 +0300

    feat: ask ai home page

--- src/components/Router/Router.tsx
+++ src/components/Router/Router.tsx
@@ -1,6 +1,7 @@
 import { Suspense } from 'react';
 import { Routes, Route, Navigate } from 'react-router-dom';
 import { Layout } from '@components/Layout';
+import { AskPage } from '@components/AskPage';
 import { routes, DEFAULT_COMMAND_PATH } from '@constants';
 import { Command } from '@pages/Command';
 import { Main } from '@pages/Main';
@@ -21,6 +22,7 @@ export const Router = () => {
           <Route index element={<Navigate to={DEFAULT_COMMAND_PATH} replace />} />
           <Route path={routes.COMMAND} element={<Command />} />
           <Route path={routes.QUESTION} element={<Question />} />
+          <Route path={routes.ASK_AI_HOME} element={<AskPage />} />
         </Route>
       </Routes>
     </Suspense>
--- src/constants/routes.ts
+++ src/constants/routes.ts
@@ -1,3 +1,6 @@
 export const MAIN = '/' as const;
 export const COMMAND = '/c/:commandAddr/a/:addr/:format' as const;
 export const QUESTION = '/q/:question/:format' as const;
+
+export const ASK_AI_HOME = '/ask/home' as const;
+export const ASK_AI_ANSWER = '/ask/answer' as const;
