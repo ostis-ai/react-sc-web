59b6f7a8daaf11cf259cdc02555b2a8fd0e0ff5a
Alexandr Rutkowski <kitanoyoru@protonmail.com>
2023-12-11 12:04:45 +0300

    Dev

--- src/api/requests/getWhatIsIMS.ts
+++ src/api/requests/getWhatIsIMS.ts
@@ -3,37 +3,33 @@ import { client, isAxiosError, scUtils } from '@api';
 
 import { doCommand } from './command';
 
-
 export const getWhatIsIMS = async () => {
-    const { uiMenuSummary } = await scUtils.findKeynodes("ui_menu_summary");
+  const { uiMenuSummary, knowledgeBaseIMS } = await scUtils.findKeynodes(
+    'ui_menu_summary',
+    'knowledge_base_IMS',
+  );
 
-    const { knowledgebaseIMS } = await scUtils.findKeynodes("knowledge_base_IMS");
+  const commandResult = await doCommand(uiMenuSummary.value, knowledgeBaseIMS.value);
 
-    const commandResult = await doCommand(uiMenuSummary.value, knowledgebaseIMS.value)
+  if (isAxiosError(commandResult)) return null;
 
-    if (isAxiosError(commandResult)) return null;
+  const questionNode = commandResult.data.question;
+  const answer = await scUtils.getAnswer(new ScAddr(questionNode));
 
-    const questionNode = commandResult.data.question;
-    const answer = await scUtils.getAnswer(new ScAddr(questionNode));
+  if (!answer) return null;
 
-    if (!answer) return null;
+  const linkAlias = '_link';
+  const linkTemplate = new ScTemplate();
+  linkTemplate.triple(answer, ScType.EdgeAccessVarPosPerm, [ScType.LinkVar, linkAlias]);
 
-    const linkAlias = '_link';
-    const linkTemplate = new ScTemplate();
-    linkTemplate.triple(answer, ScType.EdgeAccessVarPosPerm, [ScType.LinkVar, linkAlias]);
+  const linkRes = await client.templateSearch(linkTemplate);
 
-    const linkRes = await client.templateSearch(linkTemplate);
+  if (!linkRes.length) return null;
 
-    if (!linkRes.length) return null;
+  const linkAddr = linkRes[0].get(linkAlias);
 
-    const linkAddr = linkRes[0].get(linkAlias)
+  const contents = await client.getLinkContents([linkAddr]);
+  const content = contents[0].data;
 
-    const contents = await client.getLinkContents([linkAddr]);
-    const content = contents[0].data;
-
-    return JSON.parse(String(content));
-}; 
-
-
-
-
+  return String(content);
+};
