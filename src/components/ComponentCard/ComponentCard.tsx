a40f05dcbf8014acbcbe53687272c9868d5150e0
Daer0n <danilatarbaya57@gmail.com>
2024-03-03 22:14:55 +0300

    feat(CardComponent): add styles

--- src/components/ComponentCard/ComponentCard.tsx
+++ src/components/ComponentCard/ComponentCard.tsx
@@ -10,29 +10,29 @@ import GithubIcon from '../../assets/images/GithubIcon.svg';
 export const ComponentCard = () => {
   return (
     <div className={styles.container}>
-      <div className={styles.wrapper}>
+      <div className={styles.info}>
         <div className={styles.content}>
           <Interface />
         </div>
-        <div className={styles.info}>
-          <div className={styles.cardInfo}>
-            <div className={styles.infoItem}>
-              <p className={styles.title}>Name</p>
-              <div className={styles.subtitle}>knowledge base</div>
-            </div>
-            <div className={styles.infoItem}>
-              Minus qui necessitatibus ipsa et cupiditate velit consequatur blanditiis.
-            </div>
+
+        <div className={styles.cardInfo}>
+          <div className={styles.infoItem}>
+            <div className={styles.title}>Name</div>
+            <div className={styles.subtitle}>knowledge base</div>
           </div>
-          <div className={styles.infoUrl}>
-            <div className={styles.icon}>
-              <GithubIcon />
-            </div>
-            <a href="#" className={styles.installLink}>
-              Install
-            </a>
+
+          <div className={styles.infoItem}>
+            Minus qui necessitatibus ipsa et cupiditate velit consequatur blanditiis.
           </div>
         </div>
+      </div>
+
+      <div className={styles.infoUrl}>
+        <span className={styles.icon}>
+          <GithubIcon />
+        </span>
+
+        <button className={styles.installLink}>Install</button>
       </div>
     </div>
   );
