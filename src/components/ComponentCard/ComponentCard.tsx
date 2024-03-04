aa868409a4806d0d280a30d8c97713988a40cc1e
Daer0n <danilatarbaya57@gmail.com>
2024-03-04 23:40:42 +0300

    refactor(ComponentCard): dynamic component

--- src/components/ComponentCard/ComponentCard.tsx
+++ src/components/ComponentCard/ComponentCard.tsx
@@ -1,29 +1,70 @@
+import React from 'react';
 import classNames from 'classnames';
 import styles from './ComponentCard.module.scss';
+import { CardComponentType, CardComponentImageType } from './types';
 
-import Interface from '../../assets/images/DefaultPluginImages/Interface.svg';
-import KnowledgeBase from '../../assets/images/DefaultPluginImages/KnowledgeBase.svg';
-import ProblemSolver from '../../assets/images/DefaultPluginImages/ProblemSolver.svg';
-import Subsystem from '../../assets/images/DefaultPluginImages/Subsystem.svg';
-import GithubIcon from '../../assets/images/GithubIcon.svg';
+import Interface from '@assets/images/DefaultPluginImages/Interface.svg';
+import KnowledgeBase from '@assets/images/DefaultPluginImages/KnowledgeBase.svg';
+import ProblemSolver from '@assets/images/DefaultPluginImages/ProblemSolver.svg';
+import Subsystem from '@assets/images/DefaultPluginImages/Subsystem.svg';
+import GithubIcon from '@assets/images/GithubIcon.svg';
 
-export const ComponentCard = () => {
+interface ComponentCardProps {
+  subtitle: CardComponentType;
+  description: string;
+  content: CardComponentImageType;
+}
+
+export const ComponentCard: React.FC<ComponentCardProps> = ({ subtitle, description, content }) => {
+  let contentComponent: React.ReactNode;
+  let subtitleClassName = classNames(styles.subtitle, styles.defaultSubtitle);
+
+  switch (content) {
+    case CardComponentImageType.interfaceImg:
+      contentComponent = <Interface />;
+      break;
+    case CardComponentImageType.knowledgeBaseImg:
+      contentComponent = <KnowledgeBase />;
+      break;
+    case CardComponentImageType.problemSolverImg:
+      contentComponent = <ProblemSolver />;
+      break;
+    case CardComponentImageType.subSystemImg:
+      contentComponent = <Subsystem />;
+      break;
+    default:
+      contentComponent = null;
+  }
+
+  switch (subtitle) {
+    case CardComponentType.knowledgeBase:
+      subtitleClassName = classNames(styles.subtitle, styles.subtitleKnowledgeBase);
+      break;
+    case CardComponentType.subSystem:
+      subtitleClassName = classNames(styles.subtitle, styles.subtitleSubSystem);
+      break;
+    case CardComponentType.problemSolver:
+      subtitleClassName = classNames(styles.subtitle, styles.subtitleProblemSolver);
+      break;
+    case CardComponentType.interface:
+      subtitleClassName = classNames(styles.subtitle, styles.subtitleInterface);
+      break;
+    default:
+      break;
+  }
+
   return (
     <div className={styles.container}>
       <div className={styles.info}>
-        <div className={styles.content}>
-          <Interface />
-        </div>
+        <div className={styles.content}>{contentComponent}</div>
 
         <div className={styles.cardInfo}>
           <div className={styles.infoItem}>
             <div className={styles.title}>Name</div>
-            <div className={styles.subtitle}>knowledge base</div>
+            <div className={subtitleClassName}>{subtitle}</div>
           </div>
 
-          <div className={styles.description}>
-            Minus qui necessitatibus ipsa et cupiditate velit consequatur blanditiis.
-          </div>
+          <div className={styles.description}>{description}</div>
         </div>
       </div>
 
---
+++ src/components/ComponentCard/types.ts
@@ -0,0 +1,14 @@
+
+export const enum CardComponentType {
+  knowledgeBase = 'knowledge base',
+  problemSolver = 'problem solver',
+  interface = 'interface',
+  subSystem = 'subsystem',
+}
+
+export const enum CardComponentImageType {
+    knowledgeBaseImg = 'knowledgeBaseImg',
+    problemSolverImg = 'problemSolverImg',
+    interfaceImg = 'interfaceImg',
+    subSystemImg = 'subSystemImg',
+}
