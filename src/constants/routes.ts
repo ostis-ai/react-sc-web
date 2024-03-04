813acd2a11b7dbb0766989536ef1d25f8d599f55
Daer0n <danilatarbaya57@gmail.com>
2024-03-05 00:00:45 +0300

    refactor(): rename Library page

--- src/constants/routes.ts
+++ src/constants/routes.ts
@@ -1,4 +1,4 @@
 export const MAIN = '/' as const;
 export const COMMAND = '/c/:commandAddr/a/:addr/:format' as const;
 export const QUESTION = '/q/:question/:format' as const;
-export const LIBERARY = '/l/' as const;
+export const LIBRARY = '/l/' as const;
