bef747b4dd345bd70fac09c88f6c534eeed4dda8
Daer0n <danilatarbaya57@gmail.com>
2024-03-10 16:11:46 +0300

    feat(SwitchMode): add switch buttons

--- src/constants/routes.ts
+++ src/constants/routes.ts
@@ -2,3 +2,4 @@ export const MAIN = '/' as const;
 export const COMMAND = '/c/:commandAddr/a/:addr/:format' as const;
 export const QUESTION = '/q/:question/:format' as const;
 export const LIBRARY = '/l/' as const;
+export const ASK_AI = '/c/' as const;
