e8fec082ca5a9a495d4c1f9fdc28f59124e0f029
vanya <vanya.prokopovich.02@mail.ru>
2024-03-30 00:34:42 +0300

    feat: added context menu for components

--- src/components/ToolTip/ToolTip.tsx
+++ src/components/ToolTip/ToolTip.tsx
@@ -1,17 +1,49 @@
+import React from 'react';
 import { Tooltip as BaseTooltip } from 'ostis-ui-lib';
-import { ReactNode, useEffect, useState } from 'react';
+import { ReactNode, useEffect, useState, useRef } from 'react';
 import { ScLangText } from 'ostis-ui-lib';
 import { useSelector } from 'react-redux';
 import { selectDevMode } from '@store/devModeSlice';
+import styles from './Tooltip.module.scss';
+import { ScTag } from 'ostis-ui-lib';
+import { searchAddrById } from '@api/sc/search/search';
 
 interface IProps {
   children: ReactNode;
-  commandAddr: String;
+  commandAddr?: number;
+  systemId?: string;
 }
 
-export const Tooltip = ({ children, commandAddr }: IProps) => {
+export const Tooltip = ({ commandAddr, systemId, children }: IProps) => {
   const devMode = useSelector(selectDevMode);
-  const title = <ScLangText addrOrSystemId={String(commandAddr)} />;
+  const [scAddr, setScAddr] = useState<undefined | number>(undefined);
 
-  return <BaseTooltip title={title} children={children} />;
+  useEffect(() => {
+    const fetchData = async () => {
+      if (systemId !== undefined) {
+        const addr = await searchAddrById(systemId);
+        setScAddr(addr?.value || undefined);
+        console.log(scAddr);
+      }
+    };
+
+    fetchData();
+  }, [systemId]);
+
+  // если не надо рендерить компоненты без адреса if (systemId !== undefined && scAddr !== null)
+  if (systemId !== undefined) {
+    const title = <ScLangText addrOrSystemId={Number(scAddr)} />;
+    return (
+      <BaseTooltip title={title} className={styles.tooltip}>
+        <ScTag addr={scAddr}>{children}</ScTag>
+      </BaseTooltip>
+    );
+  } else if (commandAddr !== undefined) {
+    const title = <ScLangText addrOrSystemId={Number(commandAddr)} />;
+    return (
+      <BaseTooltip title={title} className={styles.tooltip}>
+        {children}
+      </BaseTooltip>
+    );
+  }
 };
