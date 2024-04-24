6a03537881c4a8a0ca401bf3e820b3ed88bbf58e
VasKho <vasya.khoroshavin@gmail.com>
2024-04-24 19:17:20 +0300

    fix: router for prod build; no-refresh SCg; html tags

--- src/constants/common.ts
+++ src/constants/common.ts
@@ -1,16 +1,11 @@
 import { generatePath } from 'react-router-dom';
 import { routes } from '@constants';
 
-const IS_DEV = process.env.NODE_ENV === 'development';
-const IS_PROD = process.env.NODE_ENV === 'production';
+const defaultApiUrl = 'http://localhost:8000';
+const defaultScUrl = 'ws://localhost:8090/ws_json';
 
-const defaultBasePath = IS_DEV ? undefined : '/react';
-// const defaultApiUrl = IS_PROD ? process.env.API_URL : '';
-const defaultApiUrl = IS_DEV ? process.env.API_URL : '';
-
-export const BASE_PATH = process.env.BASE_PATH ?? defaultBasePath;
-export const SC_URL = process.env.SC_URL!;
-export const API_URL = defaultApiUrl;
+export const SC_URL = process.env.SC_URL? process.env.SC_URL : defaultScUrl;
+export const API_URL = process.env.API_URL? process.env.API_URL : defaultApiUrl;
 
 export const DEFAULT_SYSTEM_ID = 'myself' as const;
 export const DEFAULT_COMMAND_SYSTEM_ID = 'ui_menu_view_full_semantic_neighborhood' as const;
