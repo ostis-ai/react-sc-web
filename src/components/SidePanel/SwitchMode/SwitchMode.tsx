96335f4c83ae428eb31ae3e85439751c2b396a38
NikitaZotov <nikita.zotov.belarus@gmail.com>
2025-03-12 20:43:32 +0300

    feat: disable ask ai button

--- src/components/SidePanel/SwitchMode/SwitchMode.tsx
+++ src/components/SidePanel/SwitchMode/SwitchMode.tsx
@@ -40,13 +40,13 @@ export const SwitchMode = () => {
           <ScnPageButton />
         )}
       </Link>
-      <Link
+      {/* <Link
         to={routes.ASK_AI}
         className={styles.switchModeButton}
         onClick={() => handlePageClick(routes.ASK_AI)}
       >
         {activePage === routes.ASK_AI ? <AskAiPageButtonFocus /> : <AskAiPageButton />}
-      </Link>
+      </Link> */}
       <Link
         to={routes.LIBRARY}
         className={styles.switchModeButton}
