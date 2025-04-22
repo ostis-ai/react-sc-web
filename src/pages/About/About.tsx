0673f2e8da4f34e67e6bb4bfc33b6984a1592b91
NikitaZotov <nikita.zotov.belarus@gmail.com>
2025-04-22 16:04:35 +0300

    feat: update styles for rendering md

--- src/pages/About/About.tsx
+++ src/pages/About/About.tsx
@@ -1,6 +1,9 @@
 import ReactMarkdown from 'react-markdown';
 import { useSelector } from 'react-redux';
 import remarkGfm from 'remark-gfm';
+import rehypeRaw from 'rehype-raw';
+import rehypeHighlight from 'rehype-highlight';
+import rehypeKatex from 'rehype-katex';
 import { selectCurrentAboutText } from '@store/aboutSlice';
 
 import AboutMetasystem from '@assets/docs/AboutMetasystem.md';
@@ -8,16 +11,16 @@ import AboutMetasystem from '@assets/docs/AboutMetasystem.md';
 import styles from './About.module.scss';
 
 const About = () => {
-  const currentText = useSelector(selectCurrentAboutText);
+  const currentText = useSelector(selectCurrentAboutText) || AboutMetasystem;
 
   return (
     <>
       <div className={styles.markdownContent}>
-        {currentText ? (
-          <ReactMarkdown children={currentText} remarkPlugins={[remarkGfm]} />
-        ) : (
-          <ReactMarkdown children={AboutMetasystem} remarkPlugins={[remarkGfm]} />
-        )}
+        <ReactMarkdown 
+          children={currentText} 
+          remarkPlugins={[remarkGfm]}
+          rehypePlugins={[rehypeRaw, rehypeHighlight, rehypeKatex]}
+        />
       </div>
     </>
   );
