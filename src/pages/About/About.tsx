4b28233ab731170823f4459e397b0eb8754e175f
NikitaZotov <nikita.zotov.belarus@gmail.com>
2025-04-22 18:11:03 +0300

    fix: clear about text after leaving about page

--- src/pages/About/About.tsx
+++ src/pages/About/About.tsx
@@ -1,10 +1,13 @@
 import ReactMarkdown from 'react-markdown';
+import { useEffect } from 'react';
+import { useDispatch } from 'react-redux';
 import { useSelector } from 'react-redux';
 import remarkGfm from 'remark-gfm';
 import rehypeRaw from 'rehype-raw';
 import rehypeHighlight from 'rehype-highlight';
 import rehypeKatex from 'rehype-katex';
 import { selectCurrentAboutText } from '@store/aboutSlice';
+import { clearAboutText } from '@store/aboutSlice';
 
 import AboutMetasystem from '@assets/docs/AboutMetasystem.md';
 
@@ -12,6 +15,13 @@ import styles from './About.module.scss';
 
 const About = () => {
   const currentText = useSelector(selectCurrentAboutText) || AboutMetasystem;
+  const dispatch = useDispatch();
+
+  useEffect(() => {
+    return () => {
+      dispatch(clearAboutText());
+    };
+  }, [dispatch]);
 
   return (
     <>
